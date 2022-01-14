2d7176724c050b3cd9013439dd1178e7eb864a58
Sanmi-Owoka <owokaayoola@outlook.com>
2022-01-14 21:29:53 +0100

    Added forgot password and reset password features

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,9 +3,13 @@ from .views.register import RegisterView
 from .views.email_verification import VerifyEmail
 from .views.login import LoginView
 from .views.logout import LogoutView
+from .views.forgot_password import ForgotPasswordView
+from .views.reset_password import PasswordResetView
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('verify-email', VerifyEmail.as_view(), name='verify-email'),
     path('login', LoginView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout')
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('forgot-password', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('password-reset', PasswordResetView.as_view(), name='password-reset'),
 ]
